634308303fa10ddd4a77ac6cdd31c2736e3f24a4
harshwardhan <harshw.pauranik@gmail.com>
2020-03-22 02:09:35 +0530

    feat: seat config functionality complete

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -5,6 +5,16 @@ class Ticket < ApplicationRecord
     accepts_nested_attributes_for :passenger, allow_destroy: true
 
     def generate_ticket
-        self.pnr = ""
+        split_seat = seat_number.split('.');
+        self.seat_class = split_seat[0]
+        self.seat_number = split_seat[1]
+
+        if self.seat_class == 'Business'
+            
+        elsif self.seat_class == 'Premium Economy'
+        
+        else
+            
+        end
     end
 end
